c8693b0d7f00f100324ac813550107a3f15b2298
Juji <jujibla@gmail.com>
2025-04-21 11:52:51 +0200

    Moving Platform
    added moving platform
    added parentPlayerToPlatform script to the one way platform

---
+++ Assets/Scripts/ParentPlayerToPlatform.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentPlayerToPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.transform.SetParent(transform); //parents player to platform
+
+            other.GetComponent<Rigidbody2D>().interpolation = RigidbodyInterpolation2D.None;
+            //Interpolation Interpolate makes player move very slow on moving platform
+            //none works great but gives jittery camera movement
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+
+            other.GetComponent<Rigidbody2D>().interpolation = RigidbodyInterpolation2D.Interpolate;
+        }
+    }
+}
